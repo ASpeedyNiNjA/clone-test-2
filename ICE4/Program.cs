ccd77f6db7b6e9015fa68112dbc7ce0c8b33a838
ASpeedyNiNjA <christrombly001@hotmail.com>
2025-09-26 18:56:50 -0400

    Implemented Input Validation with TryParse

--- ICE4/Program.cs
+++ ICE4/Program.cs
@@ -13,202 +13,217 @@ namespace ICE4
             while (NextKey != ConsoleKey.Q && NextKey != ConsoleKey.Escape)
             {
                 // program code here
-                NextKey = Console.ReadKey(true).Key;
-            }
+                //NextKey = Console.ReadKey(true).Key;
+                //}
 
 
 
-            // Character's Name
-            string CharacterName = "Unknown";
+                // Character's Name
+                string CharacterName = "Unknown";
 
-            // Primary Attributes
-            int Agility = 0;
-            int Strength = 0;
-            int Vigour = 0;
-            int Perception = 0;
-            int Intellect = 0;
-            int Will = 0;
+                // Primary Attributes
+                int Agility = 0;
+                int Strength = 0;
+                int Vigour = 0;
+                int Perception = 0;
+                int Intellect = 0;
+                int Will = 0;
 
-            // Secondary Attributes
-            int Awareness = 0;
-            int Toughness = 0;
-            int Resolve = 0;
+                // Secondary Attributes
+                int Awareness = 0;
+                int Toughness = 0;
+                int Resolve = 0;
 
-            // Career Variables
-            string Career = "Unknown";
-            int CareerChoice = 0;
-            bool HasChosenCareer = false;
-
-
-            int min = 5;
-            int max = 50;
+                // Career Variables
+                string Career = "Unknown";
+                int CareerChoice = 0;
+                bool HasChosenCareer = false;
 
 
-
-            // Change the Console colours
-            Console.BackgroundColor = ConsoleColor.DarkGray;
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Clear();
-
-            // Change the Console Title Bar
-            Console.Title = "Character Sheet";
-
-            // Prompt for CharacterName
-            Console.Write("Enter Character Name: ");
-            CharacterName = Console.ReadLine();
-
-            Console.WriteLine("Select your Career: ");
-            Console.WriteLine("1. Army (AGL: 35, STR: 35, VGR: 30, PER: 30, INT: 25, WIL: 25)");
-            Console.WriteLine("2. Psion (AGL: 30, STR: 35, VGR: 30, PER: 25, INT: 35, WIL: 25)");
-            Console.WriteLine("3. Rogue (AGL: 35, STR: 30, VGR: 30, PER: 35, INT: 25, WIL: 25)");
-            Console.WriteLine("4. Telepath (AGL: 25, STR: 25, VGR: 30, PER: 30, INT: 35, WIL: 35)");
-            Console.WriteLine("5. Tinker (AGL: 30, STR: 35, VGR: 25, PER: 30, INT: 35, WIL: 25)");
-            Console.WriteLine("6. Random ");
-            Console.Write("You Selection: ");
-            CareerChoice = Convert.ToInt32(Console.ReadLine());
-
-            switch (CareerChoice)
-            {
-                case 1:
-                    Career = "Army";
-                    Agility = 35;
-                    Strength = 35;
-                    Vigour = 30;
-                    Perception = 30;
-                    Intellect = 25;
-                    Will = 25;
-                    HasChosenCareer = true;
-                    break;
-
-                case 2:
-
-                    Career = "Psion";
-                    Agility = 30;
-                    Strength = 35;
-                    Vigour = 30;
-                    Perception = 30;
-                    Intellect = 35;
-                    Will = 25;
-                    HasChosenCareer = true;
-                    break;
-
-                case 3:
-
-                    Career = "Rogue";
-                    Agility = 35;
-                    Strength = 30;
-                    Vigour = 30;
-                    Perception = 35;
-                    Intellect = 25;
-                    Will = 25;
-                    HasChosenCareer = true;
-                    break;
-
-                case 4:
-
-                    Career = "Telepath";
-                    Agility = 25;
-                    Strength = 25;
-                    Vigour = 30;
-                    Perception = 30;
-                    Intellect = 35;
-                    Will = 35;
-                    HasChosenCareer = true;
-                    break;
-
-                case 5:
-
-                    Career = "Tinker";
-                    Agility = 30;
-                    Strength = 35;
-                    Vigour = 25;
-                    Perception = 30;
-                    Intellect = 35;
-                    Will = 25;
-                    HasChosenCareer = true;
-                    break;
+                int min = 5;
+                int max = 50;
 
 
-                case 6:
 
-                    Career = "Random";
-                    Random d10 = new Random();
-                    Agility = d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11);
-                    Strength = d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11);
-                    Vigour = d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11);
-                    Perception = d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11);
-                    Intellect = d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11);
-                    Will = d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11);
-                    HasChosenCareer = true;
-                    break;
-
-                default:
-                    Console.WriteLine("You entered an incorrect choice. Please run the program again.");
-                    HasChosenCareer = false;
-                    Console.WriteLine("Press any key to exit");
-                    break;
-            }     
-
-                if (HasChosenCareer)
-            { 
-                // compute secondary attributes
-                Awareness = Agility + Perception;
-                Toughness = Strength + Vigour;
-                Resolve = Intellect + Will;
-
-                // output the character sheet
+                // Change the Console colours
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.Clear();
 
-                Console.WriteLine("------------------------------------------------------");
-                Console.WriteLine($"Character Name: {CharacterName}");
-                Console.WriteLine("------------------------------------------------------");
-                Console.WriteLine($"Career        : {Career}");
-                Console.WriteLine("------------------------------------------------------");
-                Console.WriteLine("Primary Attributes");
-                Console.WriteLine("------------------------------------------------------");
-                Console.WriteLine($"Agility        : {Agility}");
-                Console.WriteLine($"Strength       : {Strength}");
-                Console.WriteLine($"Vigour         : {Vigour}");
-                Console.WriteLine($"Perception     : {Perception}");
-                Console.WriteLine($"Intellect      : {Intellect}");
-                Console.WriteLine($"Will           : {Will}");
-                Console.WriteLine("------------------------------------------------------");
-                Console.WriteLine("Secondary Attributes");
-                Console.WriteLine("------------------------------------------------------");
-                Console.WriteLine($"Awareness       : {Awareness}");
-                Console.WriteLine($"Toughness       : {Toughness}");
-                Console.WriteLine($"Resolve         : {Resolve}");
-                Console.WriteLine("------------------------------------------------------");
-                Console.WriteLine("Press Q or Ext to Exit");
-                
-                
-                /* Unfinished code section from class */
-                //NextKey = Console.ReadKey(true).Key;
-                //Console.Clear();
-                //continue;
-            }
+                // Change the Console Title Bar
+                Console.Title = "Character Sheet";
 
-            
+                // Prompt for CharacterName
+                Console.Write("Enter Character Name: ");
+                CharacterName = Console.ReadLine();
 
-            /*
-                for(int row = 0; row < 10; row++)
-            {
-                if (row == 0 || row == 9)
+                Console.WriteLine("Select your Career: ");
+                Console.WriteLine("1. Army (AGL: 35, STR: 35, VGR: 30, PER: 30, INT: 25, WIL: 25)");
+                Console.WriteLine("2. Psion (AGL: 30, STR: 35, VGR: 30, PER: 25, INT: 35, WIL: 25)");
+                Console.WriteLine("3. Rogue (AGL: 35, STR: 30, VGR: 30, PER: 35, INT: 25, WIL: 25)");
+                Console.WriteLine("4. Telepath (AGL: 25, STR: 25, VGR: 30, PER: 30, INT: 35, WIL: 35)");
+                Console.WriteLine("5. Tinker (AGL: 30, STR: 35, VGR: 25, PER: 30, INT: 35, WIL: 25)");
+                Console.WriteLine("6. Random ");
+                Console.Write("You Selection: ");
+                //CareerChoice = Convert.ToInt32(Console.ReadLine());
+
+                bool IsValid = false;
+                while (!IsValid)
                 {
-
-                    for (int col = 0; col < 10; col++)
+                    Console.Write("Enter 1 to 6: ");
+                    IsValid = int.TryParse(Console.ReadLine(), out CareerChoice);
+                    if (!IsValid)
                     {
-                        Console.Write("!");
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Invalid Entry. Try again.");
+                        Console.ForegroundColor = ConsoleColor.White;
                     }
                 }
-                else
-                {
-                    Console.Write("!        !");
-                }
-                    Console.WriteLine();
-            }
 
-            */
+                switch (CareerChoice)
+                {
+                    case 1:
+                        Career = "Army";
+                        Agility = 35;
+                        Strength = 35;
+                        Vigour = 30;
+                        Perception = 30;
+                        Intellect = 25;
+                        Will = 25;
+                        HasChosenCareer = true;
+                        break;
+
+                    case 2:
+
+                        Career = "Psion";
+                        Agility = 30;
+                        Strength = 35;
+                        Vigour = 30;
+                        Perception = 30;
+                        Intellect = 35;
+                        Will = 25;
+                        HasChosenCareer = true;
+                        break;
+
+                    case 3:
+
+                        Career = "Rogue";
+                        Agility = 35;
+                        Strength = 30;
+                        Vigour = 30;
+                        Perception = 35;
+                        Intellect = 25;
+                        Will = 25;
+                        HasChosenCareer = true;
+                        break;
+
+                    case 4:
+
+                        Career = "Telepath";
+                        Agility = 25;
+                        Strength = 25;
+                        Vigour = 30;
+                        Perception = 30;
+                        Intellect = 35;
+                        Will = 35;
+                        HasChosenCareer = true;
+                        break;
+
+                    case 5:
+
+                        Career = "Tinker";
+                        Agility = 30;
+                        Strength = 35;
+                        Vigour = 25;
+                        Perception = 30;
+                        Intellect = 35;
+                        Will = 25;
+                        HasChosenCareer = true;
+                        break;
+
+
+                    case 6:
+
+                        Career = "Random";
+                        Random d10 = new Random();
+                        Agility = d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11);
+                        Strength = d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11);
+                        Vigour = d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11);
+                        Perception = d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11);
+                        Intellect = d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11);
+                        Will = d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11) + d10.Next(1, 11);
+                        HasChosenCareer = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("You entered an incorrect choice. Please run the program again.");
+                        HasChosenCareer = false;
+                        Console.WriteLine("Press any key to exit");
+                        continue;
+                }
+
+                if (HasChosenCareer == true)
+                {
+                    // compute secondary attributes
+                    Awareness = Agility + Perception;
+                    Toughness = Strength + Vigour;
+                    Resolve = Intellect + Will;
+
+                    // output the character sheet
+                    Console.Clear();
+
+                    Console.WriteLine("------------------------------------------------------");
+                    Console.WriteLine($"Character Name: {CharacterName}");
+                    Console.WriteLine("------------------------------------------------------");
+                    Console.WriteLine($"Career        : {Career}");
+                    Console.WriteLine("------------------------------------------------------");
+                    Console.WriteLine("Primary Attributes");
+                    Console.WriteLine("------------------------------------------------------");
+                    Console.WriteLine($"Agility        : {Agility}");
+                    Console.WriteLine($"Strength       : {Strength}");
+                    Console.WriteLine($"Vigour         : {Vigour}");
+                    Console.WriteLine($"Perception     : {Perception}");
+                    Console.WriteLine($"Intellect      : {Intellect}");
+                    Console.WriteLine($"Will           : {Will}");
+                    Console.WriteLine("------------------------------------------------------");
+                    Console.WriteLine("Secondary Attributes");
+                    Console.WriteLine("------------------------------------------------------");
+                    Console.WriteLine($"Awareness       : {Awareness}");
+                    Console.WriteLine($"Toughness       : {Toughness}");
+                    Console.WriteLine($"Resolve         : {Resolve}");
+                    Console.WriteLine("------------------------------------------------------");
+                    Console.WriteLine("Press Q or Ext to Exit");
+
+
+                    /* Unfinished code section from class */
+                    NextKey = Console.ReadKey(true).Key;
+                    Console.Clear();
+                    continue;
+                }
+
+
+
+                /*
+                    for(int row = 0; row < 10; row++)
+                {
+                    if (row == 0 || row == 9)
+                    {
+
+                        for (int col = 0; col < 10; col++)
+                        {
+                            Console.Write("!");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("!        !");
+                    }
+                        Console.WriteLine();
+                }
+
+                */
+
+            }
 
         }
     }
